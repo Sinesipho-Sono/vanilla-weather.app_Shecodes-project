67eb94de1c898967732822734e5f76fe3fadeb36
Sinesipho-Sono <sinesiphobeesono@gmail.com>
2025-01-28 17:49:53 +0200

    integrated icon using API and updated css

--- src/index.js
+++ src/index.js
@@ -7,6 +7,12 @@ function getWeather(response) {
   let speedElement = document.querySelector("#speed-data");
   let timeElement = document.querySelector("#time");
   let date = new Date(response.data.time * 1000);
+  let iconElement = document.querySelector("#weather-app-icon");
+
+  iconElement.innerHTML = `<img
+      src="${response.data.condition.icon_url} "
+      class="weather-app-icon"
+    />`;
 
   city.innerHTML = response.data.city;
   timeElement.innerHTML = formatDate(date);
