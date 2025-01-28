b514ce59a7029cab407ac3fbcc859d594430a833
Sinesipho-Sono <sinesiphobeesono@gmail.com>
2025-01-28 14:28:18 +0200

    added search engine using js

---
+++ src/index.js
@@ -0,0 +1,9 @@
+function changeCityName(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector(" #search-form-input");
+  let city = document.querySelector("#city");
+  city.innerHTML = searchInput.value;
+}
+
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", changeCityName);
