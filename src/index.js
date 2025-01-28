710f74b85d350a24122fc19a6423a743c02479e0
Sinesipho-Sono <sinesiphobeesono@gmail.com>
2025-01-28 17:24:10 +0200

    intergrated more weather data from API

--- src/index.js
+++ src/index.js
@@ -2,9 +2,37 @@ function getWeather(response) {
   let temperatureElement = document.querySelector("#weather-app-temperature");
   let temperature = response.data.temperature.current;
   let city = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#weather-condition");
+  let humidityElement = document.querySelector("#humidity-data");
+  let speedElement = document.querySelector("#speed-data");
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
 
   city.innerHTML = response.data.city;
+  timeElement.innerHTML = formatDate(date);
   temperatureElement.innerHTML = Math.round(temperature);
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = ` ${response.data.temperature.humidity}% `;
+  speedElement.innerHTML = `${response.data.wind.speed}km/h`;
+}
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  if (minutes < 10) {
+    minutes = `0 ${minutes}`;
+  }
+  return ` ${day} ${hours}:${minutes}`;
 }
 
 function searchCity(city) {
